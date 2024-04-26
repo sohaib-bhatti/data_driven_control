b953acb8a6d1921ef92cbe3f77f4aaea69054d18
Sohaib Bhatti <44452143+sohaib-bhatti@users.noreply.github.com>
2024-04-26 11:05:15 -0400

    implemented SINDy_MPC for mass spring system

--- final_project/mass_spring_SINDy_c.py
+++ final_project/mass_spring_SINDy_c.py
@@ -2,32 +2,82 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import pysindy as ps
+import do_mpc
+import casadi
 
 
 def main():
     t_start = 0
-    t_stop = 200
-    x_0 = [10, -1]
-    num_samples = 100000
+    t_stop = 60
+    x0 = [5, -1]
+    num_samples = 1000
     t = np.linspace(t_start, t_stop, num=num_samples)
 
-    x = odeint(diff, x_0, t)
+    ts = (t_stop-t_start)/num_samples
 
-    # Plot the Results
+    x = odeint(diff, x0, t)
+
+    x_sindy, x_dot_sindy, model = sindy(x, t)
+
+    sim = model.simulate(x0, t=t)
+
+    model.print()
+    print("coefficients: %s", model.coefficients().T)
+
+    state_CL, u_CL = mpc_ss(model.coefficients().T, x0, ts, num_samples)
+
+    x_CL = state_CL[:, 0]
+
+    plt.figure(1)
+
     plt.plot(t, x[:, 0])
-    plt.title('mass spring')
-    plt.xlabel('t')
-    plt.ylabel('x')
+    plt.plot(t, sim[:, 0], linestyle='dashed')
+
+    plt.title('mass spring displacement')
+    plt.xlabel('t (s)')
+    plt.ylabel('x (m)')
+    plt.legend(["x", "x_sindy"])
+    plt.grid()
+
+    plt.figure(2)
+
+    plt.plot(t, x[:, 1])
+    plt.plot(t, sim[:, 1], linestyle='dashed')
+
+    plt.title('mass spring velocity')
+    plt.xlabel('t (s)')
+    plt.ylabel('x_dot (m/s)')
+    plt.legend(["x_dot", "x_dot_sindy"])
+    plt.grid()
+
+    plt.figure(3)
+
+    plt.subplot(2, 1, 1)
+    plt.plot(t, x_CL)
+    plt.title('closed loop')
+    plt.xlabel('t (s)')
+    plt.ylabel('x (m)')
     plt.legend(["x"])
     plt.grid()
+    plt.ylim([-6, 6])
+
+    plt.subplot(2, 1, 2)
+    plt.plot(t, u_CL)
+    plt.title('input force')
+    plt.xlabel('t (s)')
+    plt.ylabel('f (N/kg)')
+    plt.legend(["u"])
+    plt.grid()
+
     plt.show()
 
 
 def diff(x, t):
-    c = 4
-    k = 2
-    m = 20
-    F = 0.5 * np.sin(t)
+    c = 0.1  # Ns/m
+    k = 8  # N/m
+    m = 2  # kg
+    # F = 0.5 * np.sin(t)
+    F = 0
 
     dx1dt = x[1]
     dx2dt = (F - c*x[1] - k*x[0])/m
@@ -37,42 +87,112 @@ def diff(x, t):
 
 
 def sindy(x, t):
-    # Functions to be applied to the data x
-    functions = [lambda x: np.exp(x),
-                 lambda x: np.sin(x),
-                 lambda x: np.cos(x)]
+    optimizer = ps.optimizers.STLSQ(threshold=1e-8,
+                                    max_iter=20000)
 
-    # Functions to be applied to the data x_dot
-    x_dot_functions = [lambda x: np.exp(x),
-                       lambda x: np.sin(x),
-                       lambda x: np.cos(x)]
+    differentiation_method = ps.differentiation.FiniteDifference()
+    # pylint: disable=protected-access
+    x_dot = differentiation_method._differentiate(x, t)
 
-    # library function names includes both
-    # the x_library_functions and x_dot_library_functions names
-    function_names = [lambda x: "exp(" + x + ")",
-                      lambda x: "sin(" + x + ")",
-                      lambda x: "cos(" + x + ")",
-                      lambda x: "exp(" + x + ")",
-                      lambda x: "sin(" + x + ")",
-                      lambda x: "cos(" + x + ")",]
+    model = ps.SINDy(optimizer=optimizer,
+                     differentiation_method=differentiation_method,
+                     feature_names=["x", "x_dot"],
+                     discrete_time=False)
+    model.fit(x, t=t, ensemble=True)
 
-    lib = ps.SINDyPILibrary(library_functions=functions,
-                            x_dot_library_functions=x_dot_functions,
-                            function_names=function_names,
-                            t=t).fit(x)
+    return x, x_dot, model
 
-    lib.transform(x)
-    print("With function names: ")
-    print(lib.get_feature_names(), "\n")
 
-    sindy_library = ps.ODELibrary(
-        library_functions=functions,
-        temporal_grid=t,
-        function_names=function_names,
-        include_bias=True,
-        implicit_terms=True,
-        derivative_order=1
-    )
+def mpc(ss):
+    model = do_mpc.model.Model('continuous')
+
+    x = model.set_variable(var_type='_x', var_name='x', shape=(1, 1))
+    x_dot = model.set_variable(var_type='_x', var_name='x_dot', shape=(1, 1))
+
+    f = model.set_variable(var_type='_u', var_name='f')
+
+    model.set_rhs('dx', ss[1][0]*x + ss[1][1]*x_dot)
+    model.set_rhs('dx_dot', ss[2][0]*x + ss[2][1]*x_dot + f)
+
+    model.setup()
+
+    mpc = do_mpc.controller.MPC(model)
+
+    setup_mpc = {'n_horizon': 20,
+                 't_step': 0.1,
+                 'n_robust': 1,
+                 'store_full_solution': True, }
+    mpc.set_param(**setup_mpc)
+
+
+def mpc_ss(ss, x0, ts, num_iters):
+    model = do_mpc.model.Model('continuous')
+
+    x = model.set_variable(var_type='_x', var_name='x', shape=(2, 1))
+    u = model.set_variable(var_type='_u', var_name='u')
+
+    A = np.vstack((ss[1], ss[2]))
+    print(A)
+    print("here!")
+    B = np.array([0, 1])
+
+    x_next = A@x + B@u
+
+    model.set_rhs('x', x_next)
+
+    model.set_expression(expr_name='cost', expr=casadi.sum1(x**2))
+
+    model.setup()
+
+    mpc = do_mpc.controller.MPC(model)
+    mpc.settings.supress_ipopt_output()
+
+    setup_mpc = {'n_horizon': 20,
+                 't_step': 0.1,
+                 'n_robust': 1,
+                 'store_full_solution': True, }
+    mpc.set_param(**setup_mpc)
+
+    mterm = model.aux['cost']  # terminal cost
+    lterm = model.aux['cost']  # terminal cost
+    # stage cost
+
+    mpc.set_rterm(u=1e-4)  # input penalty
+
+    # set bounds
+    mpc.bounds['lower', '_x', 'x'] = -20
+    mpc.bounds['upper', '_x', 'x'] = 20
+    mpc.bounds['lower', '_u', 'u'] = -20
+    mpc.bounds['upper', '_u', 'u'] = 20
+
+    mpc.set_objective(mterm=mterm, lterm=lterm)
+
+    mpc.setup()
+    estimator = do_mpc.estimator.StateFeedback(model)
+    simulator = do_mpc.simulator.Simulator(model)
+
+    params_simulator = {'integration_tool': 'idas',
+                        'abstol': 1e-8,
+                        'reltol': 1e-8,
+                        't_step': ts}
+
+    simulator.set_param(**params_simulator)
+    simulator.setup()
+
+    x0 = np.array([5, -1])
+
+    mpc.x0 = x0
+    simulator.x0 = x0
+    estimator.x0 = x0
+
+    mpc.set_initial_guess()
+
+    for k in range(num_iters):
+        u0 = mpc.make_step(x0)
+        y_next = simulator.make_step(u0)
+        x0 = estimator.make_step(y_next)
+
+    return mpc.data['_x'], mpc.data['_u']
 
 
 if __name__ == '__main__':
